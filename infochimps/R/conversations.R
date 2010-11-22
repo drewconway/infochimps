ee80595b9192b91068cfd51658b4666eb4b0e6bd
Drew Conway <agconway@gmail.com>
2010-11-22 12:47:47 -0500

    Fixed two bugs:
    1. fromJSON was returning improperly parsed conversation IDs, so added regex to fix JSON before parsing
    2. Added exception handling for when no "repy-to" data exists

--- infochimps/R/conversations.R
+++ infochimps/R/conversations.R
@@ -17,13 +17,21 @@ function(screen.name.a,screen.name.b,session,user.id.a=NA,user.id.b=NA) {
         }
     }
     conversation.get<-getURL(conversation.url)
+    # Fix JSON for proper handling for conversation IDs
+    conversation.get<-gsub("([0-9]+)","\\\"\\1\\\"\\2",conversation.get,perl=TRUE)
     conversation.data<-fromJSON(conversation.get)
     # Simple error checking
     if(is.null(conversation.data$error)) {
         user.id.a<-conversation.data$user_a_id[[1]]
         user.id.b<-conversation.data$user_b_id[[1]]
         conversations.matrix<-suppressWarnings(do.call("rbind", conversation.data$conversations))
-        reply.to<-sapply(1:nrow(conversations.matrix), function(x) ifelse(conversations.matrix[x,2]=="re", conversations.matrix[x,3], NA))
+        if(dim(conversations.matrix)[2]<3) {
+            # JSON request returns no reply-to data
+            reply.to<-NA
+        }
+        else {
+            reply.to<-sapply(1:nrow(conversations.matrix), function(x) ifelse(conversations.matrix[x,2]=="re", conversations.matrix[x,3], NA))
+        }
         conversations.df<-cbind(user.id.a, user.id.b, conversations.matrix[,1], conversations.matrix[,2],reply.to)
         conversations.df<-as.data.frame(conversations.df)
         names(conversations.df)<-c("user.id.a","user.id.b","conversation.id","conversation.type","reply.to.id")
